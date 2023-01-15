33b88899c599e02f4b5aab8b120f280651cb2129
Gwestone <Gwestonem@gmail.com>
2023-01-15 12:27:59 +0200

    fourth section completed

---
+++ components/Community/Community.tsx
@@ -0,0 +1,106 @@
+import { inspect } from "util";
+import styles from "./Community.module.scss";
+import Image from "next/image";
+
+export default function Community() {
+  return (
+    <div className={styles.base}>
+      <div className={styles.container}>
+        <div className={styles.sectionSubTitle}>Our Community</div>
+        <div className={styles.sectionTitle}>Community Main Feature</div>
+        <div className={styles.commnityText}>
+          The wise man therefore always holds in these matters to this principle
+          of selection.
+        </div>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Image
+                src={"./communityIcons/people.svg"}
+                alt={"people"}
+                height={105}
+                width={105}
+              />
+            </div>
+            <div className={styles.cartTitle}>Members, Friends</div>
+            <div className={styles.cardText}>
+              Members, Friends Connection ( like followers ), Private Message
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Image
+                src={"./communityIcons/people2.svg"}
+                alt={"people"}
+                height={105}
+                width={105}
+              />
+            </div>
+            <div className={styles.cartTitle}>Groups</div>
+            <div className={styles.cardText}>
+              Your users can create groups to let other users to join and talk
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Image
+                src={"./communityIcons/chat.svg"}
+                alt={"people"}
+                height={105}
+                width={105}
+              />
+            </div>
+            <div className={styles.cartTitle}>Forum</div>
+            <div className={styles.cardText}>
+              Forum is ready by BBPress. Your users can make topics and talk.
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Image
+                src={"./communityIcons/puzzle.svg"}
+                alt={"people"}
+                height={105}
+                width={105}
+              />
+            </div>
+            <div className={styles.cartTitle}>Custom Module</div>
+            <div className={styles.cardText}>
+              You can create Members, Groups Module. We already created 3
+              modules. It's by drag & drop live builder.
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Image
+                src={"./communityIcons/checkList.svg"}
+                alt={"people"}
+                height={105}
+                width={105}
+              />
+            </div>
+            <div className={styles.cartTitle}>List Builder</div>
+            <div className={styles.cardText}>
+              Members, Groups list can be modified by drag & drop live builder.
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Image
+                src={"./communityIcons/list.svg"}
+                alt={"people"}
+                height={105}
+                width={105}
+              />
+            </div>
+            <div className={styles.cartTitle}>List Scroll Effects</div>
+            <div className={styles.cardText}>
+              8 different scroll effects are ready. You can always change by
+              your tastes.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Headline from "../components/Headline/Headline";
 import Description from "../components/Description/Description";
 import Achievements from "../components/Achievements/Achievements";
+import Community from "../components/Community/Community";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Headline />
       <Description />
       <Achievements />
+        <Community />
     </>
   );
 }
